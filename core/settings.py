bd7e9ba1a18533fc476ba1bd1270ef01441004b9
kimdir <turaqulovfayoz4@gmail.com>
2025-09-13 14:45:14 +0500

    new

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = "django-insecure-0r=*z-@#e=@qj#*_iz0_-n#*8sm)f2lwznk1*)r*@3g&w@grj_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
